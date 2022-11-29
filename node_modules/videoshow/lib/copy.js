59c306884fc819e1f7961f49afb6be150e5499eb
Luke McCrea <90562614+lukemccrea@users.noreply.github.com>
2022-11-29 11:07:13 -0600

    Delete node_modules directory

--- node_modules/videoshow/lib/copy.js
+++
@@ -1,8 +0,0 @@
-var fs = require('fs')
-
-module.exports = function copy(src, dest, cb) {
-  fs.createReadStream(src)
-    .on('error', cb)
-    .on('end', cb)
-    .pipe(fs.createWriteStream(dest))
-}
